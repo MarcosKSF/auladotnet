eb27ef278bf405f95e262d061f5ead1b70d01dfd
marcosksf <marcoskaleu3@gmail.com>
2021-11-17 22:27:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace auladotnet
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+            int nois =3;
+            string[] lista = new string[10];
+            //Console.WriteLine("Digite o nome do jogador");
+
+
+            for (int i = 0; i < nois; i++)
+            {
+                Console.WriteLine("Digite o nome do jogador");
+                lista[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("A lista de jogadores é:");
+
+            for (int i = 0; i < nois; i++)
+            {
+                Console.WriteLine($" { lista[i]}");
+            }
+          
+
+
+        }
+    }
+}
+//            for (int i = 0; i<=lista.Length; i++)
